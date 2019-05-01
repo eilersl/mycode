573954bcaf07c843fde8951cf015a59902b728ad
Mona Lisa <monalisa@alta3.com>
2019-05-01 11:01:09 -0400

    Commit 05012019 1101EDT

---
+++ soliloquy/solilo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+with open("sol.txt", "r") as f:
+    data = f.read().replace("\n", " ")
+    #print(data)
+    x = data.split(" ")
+    #print(x)
+    count_list = []
+    for i in x:
+       if i not in count_list:
+           count_list.append(i)
+    print(count_list)
+
+    counted = {}
+
+# put data in counted {}
+   
+# iterate through a list
+    for item in count_list:
+        num = {item: x.count(item)} 
+        counted.update(num)
+#   print(counted)
+
+v = input("Which word do you want to see the number of times it occurs?: ")
+print(counted[v])
